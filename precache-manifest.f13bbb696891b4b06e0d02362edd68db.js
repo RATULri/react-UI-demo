51f5c49445304ba7b485f19934e7198823b92bcf
RATULri <rafiulratul21@gmail.com>
2020-08-12 04:44:49 +0600

    Updates

---
+++ precache-manifest.f13bbb696891b4b06e0d02362edd68db.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd750a4c1c168eb6f392c230cdafcafb",
+    "url": "/react-UI-demo/index.html"
+  },
+  {
+    "revision": "27ba795d7b1d21528671",
+    "url": "/react-UI-demo/static/css/main.d79aec34.chunk.css"
+  },
+  {
+    "revision": "d9f015a51e59dd73f3f6",
+    "url": "/react-UI-demo/static/js/2.08f533ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-UI-demo/static/js/2.08f533ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ba795d7b1d21528671",
+    "url": "/react-UI-demo/static/js/main.9bd0f9e5.chunk.js"
+  },
+  {
+    "revision": "e6d3a3fce39d26839be9",
+    "url": "/react-UI-demo/static/js/runtime-main.e1db0738.js"
+  },
+  {
+    "revision": "bfa9612780402deab902e1f52db42c8a",
+    "url": "/react-UI-demo/static/media/chat.bfa96127.png"
+  },
+  {
+    "revision": "5afe85d69f2d0d1cbd679ee2e6dfde45",
+    "url": "/react-UI-demo/static/media/clock.5afe85d6.png"
+  },
+  {
+    "revision": "95bbf9e3715c3992964e68f0408d1a01",
+    "url": "/react-UI-demo/static/media/loupe.95bbf9e3.png"
+  },
+  {
+    "revision": "bdbf2d5bdbfdc41f4f17cf0186ee5e3c",
+    "url": "/react-UI-demo/static/media/man (1).bdbf2d5b.jpg"
+  },
+  {
+    "revision": "6afae92563893a1fee16e3667961453c",
+    "url": "/react-UI-demo/static/media/man (2).6afae925.jpg"
+  },
+  {
+    "revision": "e61e90b275e7fe80593382ac09a96332",
+    "url": "/react-UI-demo/static/media/man (3).e61e90b2.jpg"
+  },
+  {
+    "revision": "0a363d17313a3295778230824d56e891",
+    "url": "/react-UI-demo/static/media/paper-clip.0a363d17.png"
+  },
+  {
+    "revision": "0a42dfadb7bbe490e811e2229d587631",
+    "url": "/react-UI-demo/static/media/profile.0a42dfad.jpg"
+  },
+  {
+    "revision": "9a4f428b1f089b19bb0bd67c6ba2efac",
+    "url": "/react-UI-demo/static/media/upload.9a4f428b.png"
+  },
+  {
+    "revision": "12a4c773a7c6211a71b621756587eb4e",
+    "url": "/react-UI-demo/static/media/user.12a4c773.png"
+  },
+  {
+    "revision": "3d3ca0361bc15da67bf7de4b34168764",
+    "url": "/react-UI-demo/static/media/woman (1).3d3ca036.jpg"
+  },
+  {
+    "revision": "64b27857b0ea159440f9df3eab9fae30",
+    "url": "/react-UI-demo/static/media/woman (2).64b27857.jpg"
+  },
+  {
+    "revision": "045acd4096663da7d2b72a41479351ed",
+    "url": "/react-UI-demo/static/media/woman.045acd40.jpg"
+  }
+]);
